9b8b26a4ab4d22758bf0b0a659b2d7fa4d1623f4
enesland <122235406+enesland@users.noreply.github.com>
2023-01-29 23:09:53 +0300

    virus.py

---
+++ virus.py
@@ -0,0 +1,53 @@
+import os
+
+os.system("figlet E-virus ")
+print(">>>CODED BY ENESLAND root1.1<<<")
+
+print("""
+1-) İNFECT KULLAN
+2-) Z-Virus KULLAN
+3-) GEN VİRUS KULLAN
+4-) HXP-Ducky KULLAN
+5-) Vaim KULLAN
+""")
+os.system("figlet hacked")
+islemno=input("İslem numarasi gir: ")
+
+if islemno=="1":
+        os.system("git clone https://github.com/noob-hackers/infect ")
+        print("""𝐊𝐔𝐑𝐔𝐋𝐔𝐌 𝐈𝐂𝐈𝐍 𝐊𝐎𝐃
+                 • cd
+                 • ls
+                 • cd infect
+                 • ls
+                 • bash infect.sh""")
+
+elif islemno=="2":
+        os.system("git clone https://github.com/ZechBron/zVirus-Gen")
+        print("""𝐊𝐔𝐑𝐔𝐋𝐔𝐌 𝐈𝐂𝐈𝐍 𝐊𝐎𝐃
+                 • cd zVirus-Gen
+                 • chmod +x setup.sh
+                 • ./setup.sh
+                 • ./zVirus""")
+elif islemno=="3":
+        os.system("git clone git://github.com/Ign0r3dH4x0r/GenVirus.git")
+        print("""𝐊𝐔𝐑𝐔𝐋𝐔𝐌 𝐈𝐂𝐈𝐍 𝐊𝐎𝐃
+                • cd GenVirus
+                • bash GenVirus.sh""")
+
+elif islemno=="4":
+        os.system("git clone https://github.com/hackerxphantom/HXP-DUCKY")
+        print("""𝐊𝐔𝐑𝐔𝐋𝐔𝐌 𝐈𝐂𝐈𝐍 𝐊𝐎𝐃
+                • cd
+                • cd HXP-DUCKY
+                • ls
+                • bash hxp_ducky.sh""")
+
+elif islemno=="5":
+        os.system("git clone https://github.com/VaimpierOfficial/Vaim-RamSom")
+        print("""𝐊𝐔𝐑𝐔𝐋𝐔𝐌 𝐈𝐂𝐈𝐍 𝐊𝐎𝐃
+                • cd Vaim-RamSom
+                • bash setup.sh
+                • bash start.sh""")
+else:
+        print("yanlış tuşa bastın veya çıktın")
